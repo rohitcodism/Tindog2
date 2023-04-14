0be9a72b66578ca00d83764ec2b4c436fc1e9694
Rohit <rohitpaulhhs04@gmail.com>
2023-04-14 09:09:22 +0530

    do it

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,7 @@
+function nextsequence(){
+var randomNumber = Math.floor(4*Math.random());
+buttonColours = ["red", "blue", "green", "yellow"];
+var randomChosenColour = buttonColours[randomNumber];
+gamePattern = [];
+gamePattern.push(randomChosenColour)
+}
--- jQuery/jqprc1.js
+++ jQuery/jqprc1.js
@@ -5,4 +5,11 @@ $(document).keypress(function(event) {
         $("h1").css("font-size", "10rem");
 
     });
-$
+$("h1").on("mouseover", function() {
+       setTimeout(function(){
+        $("h1").css("color", "skyblue");
+    }, 1);
+});
+$("button").on("click", function(){
+    $("h1").slideUp().slideDown().animate({opacity:.5});
+});
