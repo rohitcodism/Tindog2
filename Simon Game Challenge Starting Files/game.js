3624d8ed0b0361a0a25c1d48472050a53f06fe92
Rohit <rohitpaulhhs04@gmail.com>
2023-04-17 15:36:42 +0530

    do it

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -3,5 +3,25 @@ var randomNumber = Math.floor(4*Math.random());
 buttonColours = ["red", "blue", "green", "yellow"];
 var randomChosenColour = buttonColours[randomNumber];
 gamePattern = [];
-gamePattern.push(randomChosenColour)
+var num = gamePattern.push(randomChosenColour)
+$(".btn").fadeOut(100).fadin
+function makesound(num){
+    if(num == "blue"){
+        var blue = new Audio("sounds/blue.mp3")
+        blue.play()
+        console.log(num)
+    }else if(num == "red"){
+        var red = new Audio("sounds/red.mp3")
+        red.play()
+        console.log(num)
+    }else if(num == "green"){
+        var green = new Audio("sounds/green.mp3")
+        green.play()
+        console.log(num)
+    }else if(num == "yellow"){
+        var yellow = new Audio("sounds/yellow.mp3")
+        yellow.play()
+        console.log(num)
+    }
+}
 }
