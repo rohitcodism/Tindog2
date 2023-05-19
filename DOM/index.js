c4ac7bd61b7eab8a1a9df3c390bb41c65cbf5239
rohitcodism <rohitpaula18i@gmail.com>
2023-05-19 07:53:19 +0530

    do it

--- DOM/index.js
+++
@@ -1,3 +0,0 @@
-alert("hello!")
-heading.innerHTML = "Hello World!";
-document.querySelector("body").appendChild(heading);
---
+++ DOM/practices/p1/index.js
@@ -0,0 +1,5 @@
+console.log("This Is dom manupulation");
+let a = confirm("Do You accept this contract ?");
+console.log(a);
+let e = prompt("Type your name please : ");
+console.log(e);
