692e5171866e191d01df06803b00f7293b61f40c
rohitcodism <rohitpaula18i@gmail.com>
2023-06-11 19:00:37 +0530

    do it

---
+++ DOM/practices/p2/index.js
@@ -0,0 +1,8 @@
+document.querySelectorAll("li a")[0].style.color = "blue"
+document.body.style.backgroundColor = "skyBlue"
+document.body.style.fontFamily = "arial"
+document.body.style.color = "white"
+document.querySelector("ul button").style.backgroundColor = "red"
+document.getElementsByClassName(".netlist").style.fontFamily = "Impact,Charcoal,sans-serif"
+document.querySelector("ul button").style.borderRadius = "50px"
+document.querySelector("h1").classList.add("huge")
