d6e6eddeff6511cd69871e8b9029aaa5e516aaa9
rohitcodism <rohitpaula18i@gmail.com>
2023-06-15 09:10:31 +0530

    do it

---
+++ DOM/practices/p5/dist/index.js
@@ -0,0 +1,5 @@
+document.querySelector("h1").style.color = "red"
+document.querySelector("h1").innerHTML = "<em>Hello It's Rohit</em>"
+document.querySelector("h1").innerHTML = "<strong>Hi Guys</strong>"
+document.querySelector("ul li").style.color = "blue"
+document.querySelector("li")[1].style.color = "red"
