76314a4d7b799e5da872323c57efa39ebabd74ee
rohitcodism <rohitpaula18i@gmail.com>
2023-06-19 12:14:33 +0530

    do it

--- react/practices/learnings/src/App.js
+++ react/practices/learnings/src/App.js
@@ -3,13 +3,14 @@ let name = "Rohit"
 function App() {
   return (
     <>
-      <nav>
+      <nav className='flex-col text-red-500'>
         <li>Home</li>
         <li>About</li>
-        <li>Contact</li>
+        <li className='fl'>Contact</li>
+        </nav>
         <h1>Hello It's {name}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, modi deserunt iste odit nostrum nulla explicabo minus suscipit eveniet corporis error impedit voluptate quas expedita, quod sit molestias! Aliquam, sequi perspiciatis vel ducimus, delectus atque repellendus tenetur earum corporis nemo molestias corrupti non! Consectetur delectus distinctio ex earum pariatur, minus ducimus reprehenderit sequi quia animi at. Sed aut iste pariatur.</p>
-      </nav>
+        <input type="password" />
     </>
   );
 }
