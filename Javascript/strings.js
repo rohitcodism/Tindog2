ad161e0da4f705339fd3eae033fdc42a34532dda
rohitcodism <rohitpaula18i@gmail.com>
2023-06-23 08:05:40 +0530

    do it

---
+++ Javascript/strings.js
@@ -0,0 +1,14 @@
+// let name = 'Rohit';
+// console.log(name.length);
+// console.log(name[0]);
+// let boy1 = "Rohit";
+// let boy2 = "Anish";
+// let sentence = `${boy2} is a friend of ${boy1}`;
+// console.log(sentence);
+
+// escape sequence //
+
+let fruit = 'bana\nna';
+console.log(fruit);
+let phone = "app\"le";
+console.log(phone);
