33491e4f253d3d86a9c44ebc353a044354cead4e
rohitcodism <rohitpaula18i@gmail.com>
2023-06-28 15:56:50 +0530

    do it

--- nodejs/2.2 Native Modules/index.js
+++ nodejs/2.2 Native Modules/index.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
-fs.writeFile("message.txt", "Hello from nodejs", (err)=>{
+// fs.writeFile("message.txt", "Hello from nodejs", (err)=>{
+//     if(err) throw err;
+//     console.log("the file has been saved")
+// });
+fs.readFile('./message.txt', "utf8", (err,data)=>{
     if(err) throw err;
-    console.log("the file has been saved")
-});
+    console.log(data)
+})
---
+++ nodejs/2.2 Native Modules/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+fs.writeFile("message.txt", "Hello from nodejs", (err)=>{
+//     if(err) throw err;
+//     console.log("the file has been saved")
+// });
