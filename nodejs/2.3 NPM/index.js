bb2e18a74ea08cf442c240ebab07a2c17b46d52c
rohitcodism <rohitpaula18i@gmail.com>
2023-06-29 08:48:36 +0530

    do it

--- nodejs/2.3 NPM/index.js
+++ nodejs/2.3 NPM/index.js
@@ -2,3 +2,6 @@
 // import generateName from "sillyname"
 // var sillyName = generateName();
 // console.log('My name is ' + sillyName)
+import superheroes from "superheroes";
+var superName = superheroes.random();
+console.log("My name is " + superName)
