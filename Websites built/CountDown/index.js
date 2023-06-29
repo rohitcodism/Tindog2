01623484f78d63890ac052168ab35a18836a0e35
rohitcodism <rohitpaula18i@gmail.com>
2023-06-29 21:59:56 +0530

    do it

--- Websites built/CountDown/index.js
+++ Websites built/CountDown/index.js
@@ -2,7 +2,7 @@
 const val = document.querySelectorAll(".inputTime input")
 let endDate = 0;
 document.querySelector(".start").addEventListener("click", function(){
-    endDate = val[0].value + " " + val[1].value + " " +  val[2].value + val[3].value + ":" + val[4].value  + " " + val[5].value
+    endDate = val[0].value + " " + val[1].value + " " +  val[2].value + " " + val[3].value + ":" + val[4].value  + " " + val[5].value
 });
 console.log(endDate)
 let input = document.querySelectorAll(".col input")
@@ -11,10 +11,12 @@ function clock(){
     let end = new Date(endDate);
     let diff = (end - now);
 // days conversion //
-    input[1].value = Math.floor(((diff)/3600000)%24)
-    input[0].value = Math.floor((((((diff)/1000)/60)/60)/24));
-    input[2].value = Math.floor((((diff)/60000)%60))
-    input[3].value = Math.floor(((diff/1000)%60))
+    input[0].value = Math.floor(((((diff/3600000)/24)/30)/12));
+    input[1].value = Math.floor((((diff/3600000)/24)%30))
+    input[3].value = Math.floor(((diff)/3600000)%24);
+    input[2].value = Math.floor((((((diff)/1000)/60)/60)/24));
+    input[4].value = Math.floor((((diff)/60000)%60));
+    input[5].value = Math.floor(((diff/1000)%60));
 }
 // initial call
 clock();
