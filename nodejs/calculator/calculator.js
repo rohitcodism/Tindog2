48420f84810131d583e0827cac212b24f474ba93
rohitcodism <rohitpaula18i@gmail.com>
2023-06-30 19:11:30 +0530

    do it

--- nodejs/calculator/calculator.js
+++ nodejs/calculator/calculator.js
@@ -3,11 +3,10 @@ const app = express();
 
 
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/index.html");
     console.log(__dirname);
 });
 
 
-app.listen("3000", ()=>{
-    console.log("Server has been started at 3000.")
+app.listen(3000, ()=>{
+    console.log("Server has been started at 3000.");
 });
