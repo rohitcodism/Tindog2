5d9076749b292d6f406e89c922f5b87324e228e8
rohitcodism <rohitpaula18i@gmail.com>
2023-06-30 09:30:35 +0530

    do it

--- nodejs/Expressjs/server.js
+++ nodejs/Expressjs/server.js
@@ -11,6 +11,23 @@ app.get("/",(request, response)=>{
     response.send("<h1><em>Hello World</em></h1>");
 });
 
+
+// code for the contact route to recieve request and send responses in the contact route
+app.get("/contact", (req, res)=>{
+    res.send("Contact me on Linkedin.")
+});
+
+//code for the about route
+app.get("/about", (req, res)=>{
+    res.send("Hi I'm Rohit Paul, an aspiring Data Scientist.")
+});
+
+//code for the shop route
+app.get("/shop", (req, res)=>{
+    res.send("Here You can do your shopping");
+});
+
+
 // tunes our server at a specified port 3000
 app.listen(3000, ()=>{
     console.log("server started on port 3000");
