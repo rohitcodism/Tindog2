8d8ab39104040974de6387f2977b78405f6b61a7
rohitcodism <rohitpaula18i@gmail.com>
2023-06-30 21:00:31 +0530

    do it

--- nodejs/calculator/calculator.js
+++ nodejs/calculator/calculator.js
@@ -13,11 +13,27 @@ app.get("/", (req, res)=>{
 
 
 app.post("/", (req, res)=>{
-    let num1 = req.body.num1;
-    let num2 = req.body.num2;
-    
-    let result = num1 + num2;
-    res.send(result);
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let operation = req.body.operation;
+    let result;
+    switch(operation){
+        case '+':
+            result = num1 + num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        default:
+            result = "Error no operation entered !!"    
+    }
+    res.send("The result is : " + result);
 });
 
 
