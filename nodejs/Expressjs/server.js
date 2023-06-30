7d8c686fd171eb055b2a691f4b357378b979d89e
rohitcodism <rohitpaula18i@gmail.com>
2023-06-30 09:08:39 +0530

    do it

--- nodejs/Expressjs/server.js
+++ nodejs/Expressjs/server.js
@@ -1,5 +1,17 @@
+import { log } from "console";
 import express from "express";
+
+
 const app = express();
+
+//app.get(), function that determines what to do when our server gets a request from a browser
+//the callback function tells the server what to do after the request 
+app.get("/",(request, response)=>{
+    console.log(request);
+    response.send("<h1><em>Hello World</em></h1>");
+});
+
+// tunes our server at a specified port 3000
 app.listen(3000, ()=>{
     console.log("server started on port 3000");
 });
