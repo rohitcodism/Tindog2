8bf228e8665f587cbc7c7e8083959733d97d47ca
rohitcodism <rohitpaula18i@gmail.com>
2023-07-01 11:11:58 +0530

    do it

--- api/weather/server.js
+++ api/weather/server.js
@@ -1,2 +1,22 @@
 const express = require('express');
+const https = require('https');
 const app = express();
+
+
+app.get("/", (req, res)=>{
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=a6f1888ad9de1b93445f018986948496'
+    https.get(url, (resp)=>{
+        console.log(resp);
+    });
+
+    resp.on('data', (data)=>{
+        console.log(data);
+    });
+
+    res.send("Hello World!!");
+});
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running at port : 3000.")
+});
