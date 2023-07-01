29deb61278d0d7c36f30ca61e24670ac16f284bd
rohitcodism <rohitpaula18i@gmail.com>
2023-07-01 07:15:10 +0530

    Good Morning

--- nodejs/bmi/server.js
+++ nodejs/bmi/server.js
@@ -3,8 +3,20 @@ const app = express();
 const pass = require('body-parser');
 
 
+app.use(pass.urlencoded({extended: true}));
+
+
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post('/', (req, res)=>{
+    let weight = req.body.weight;
+    let height = req.body.height;
+
+    let bmi = (weight/(height*height));
+    res.send("Your BMI is : " + bmi);
 });
 
 
