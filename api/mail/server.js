de4c16e910c963648d45dc28ab54ba0d2ea1d4d1
rohitcodism <rohitpaula18i@gmail.com>
2023-07-03 09:29:52 +0530

    do it

--- api/mail/server.js
+++ api/mail/server.js
@@ -21,37 +21,61 @@ app.get("/", (req, res)=>{
 });
 
 app.post("/", (req, res)=>{
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const email = req.body.email;
-    let data  = {
-        members:[
-            {
-                email_address: email,
-                status: "Subscribed",
-                marge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName
-                }
-            }
-        ]
-    };
+    const fName = req.body.firstName;
+    const lName = req.body.lastName;
+    const mail = req.body.email;
+    const listId = "72683f5ee6";
+    const subscribingUser = {
+    firstName: fName,
+    lastName: lName,
+    email: mail
+};
+
+async function run() {
+    const response = await mailchimp.lists.addListMember(listId, {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: subscribingUser.firstName,
+            LNAME: subscribingUser.lastName
+        }
+    });
+
+    console.log(
+        `Successfully added contact as an audience member. The contact's id is ${response.id
+        }.`
+    );
+}
+
+run();
+//     let data  = {
+//         members:[
+//             {
+//                 email_address: email,
+//                 status: "Subscribed",
+//                 marge_fields: {
+//                     FNAME: firstName,
+//                     LNAME: lastName
+//                 }
+//             }
+//         ]
+//     };
 
     
     
-    const jsonData = JSON.stringify(data);
+//     const jsonData = JSON.stringify(data);
     
-    const url = "https://us21.api.mailchimp.com/3.0/lists/72683f5ee6"
+//     const url = "https://us21.api.mailchimp.com/3.0/lists/72683f5ee6"
     
-    const options = {
-        method: "POST",
-        auth: "rohitpa1:70f77890b2802f6c575731c9e7493cde-us21"
-    }
-    const request = https.request(url, options, (req, resp)=>{
-        response.on("data", (data)=>{
-            console.log(JSON.parse(data));
-        });
-    });
+//     const options = {
+//         method: "POST",
+//         auth: "rohitpa1:70f77890b2802f6c575731c9e7493cde-us21"
+//     }
+//     const request = https.request(url, options, (req, resp)=>{
+//         response.on("data", (data)=>{
+//             console.log(JSON.parse(data));
+//         });
+//     });
     
 });
 
