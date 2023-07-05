482e900b72429bae32f483b5cdaf8d6a12ae41b3
rohitcodism <rohitpaula18i@gmail.com>
2023-07-05 16:55:36 +0530

    do it

--- react/practices/r1/src/app.js
+++ react/practices/r1/src/app.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const App = ()=>{
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  );
+    return(
+        <>
+            <h1>App</h1>
+        </>
+    );
 };
 
 export default App;
