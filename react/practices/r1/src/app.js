dfce2e69787862b6235ceb5f87b4f26be11bbd92
rohitcodism <rohitpaula18i@gmail.com>
2023-07-05 20:38:03 +0530

    do it

--- react/practices/r1/src/app.js
+++ react/practices/r1/src/app.js
@@ -1,14 +1,25 @@
 import React, { useEffect } from "react";
 import './app.css';
 import searchIcon from './search.svg';
+import MovieCard from "./MovieCard";
 
 const URL = "https://www.omdbapi.com?apikey=a78afb62"
+
+const movie1 = 
+    {
+        "Title": "Batman Begins",
+        "Year": "2005",
+        "imdbID": "tt0372784",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+    };
+
 
 const App = ()=>{
     const searchMovies = async (title)=>{
         const response = await fetch(`${URL}&s=${title}`);
         const data = response.json();
-        console.log(data.Search);
+        console.log(data);
     };
     useEffect(()=>{
         searchMovies('Batman');
@@ -32,7 +43,7 @@ const App = ()=>{
             </div>
 
             <div className="container">
-                
+                <MovieCard movie1={movie1} />
             </div>
         </div>
     );
