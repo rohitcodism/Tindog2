505ad9ab76b1b67e212dbbb00145f63c67dfddde
rohitcodism <rohitpaula18i@gmail.com>
2023-07-06 11:25:35 +0530

    do it

--- react/practices/Portfolio/src/components/Navbar.jsx
+++ react/practices/Portfolio/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           {navLinks.map((link)=>(
             <li
             key={link.id}
-            className={`${active === link.id} ? text-white : text-secondary`}
+            className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}
             >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
