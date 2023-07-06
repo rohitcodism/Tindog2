650e7ee8181d8bae1052b38a26fc6ce94f8b05e4
rohitcodism <rohitpaula18i@gmail.com>
2023-07-06 08:52:27 +0530

    do it

--- react/practices/Portfolio/src/App.jsx
+++ react/practices/Portfolio/src/App.jsx
@@ -3,9 +3,23 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 
 const App = ()=> {
   return (
-    <>
-      3D Developer Portfolio
-    </>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
