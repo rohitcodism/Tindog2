f6134f503663ee72f0c37bedd44d8982e709e5df
rohitcodism <rohitpaula18i@gmail.com>
2023-07-06 19:59:27 +0530

    do it

--- react/practices/Portfolio/src/components/Hero.jsx
+++ react/practices/Portfolio/src/components/Hero.jsx
@@ -12,7 +12,13 @@ const Hero = () => {
             <div className="w-1 sm:h-80 h-40 violet-gradient ml-2"></div>
           </div>
         </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className="text-[#915eff]">Rohit</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I am a frontend developer and <br className="sm:block hidden" /> an aspiring Data Scientist</p>
+        </div>
       </div>
+      
+      <ComputersCanvas />
     </section>
   );
 };
