3ff88f3fd605b2070a7721b406fb80588b63713c
rohitcodism <rohitpaula18i@gmail.com>
2023-07-06 21:44:27 +0530

    do it

--- react/practices/Portfolio/src/components/canvas/Computers.jsx
+++ react/practices/Portfolio/src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = (isMobile) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
@@ -19,7 +19,7 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
+        scale={ isMobile ? 0.4 : 0.75}
         position={[0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -31,18 +31,18 @@ const ComputersCanvas = ()=>{
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(()=>{
-    const mediaQuery = window.matchMedia('max-width: 500');
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
     setIsMobile(mediaQuery.matches);
 
-    const handleChange = (event)=>{
+    const handleMediaQueryChange = (event)=>{
       setIsMobile(mediaQuery.matches);
     }
-    mediaQuery.addEventListener('change', handleChange)
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
 
     return ()=>{
-      mediaQuery.removeEventListener('change', handleChange);
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
     }
-  })
+  }, [])
 
   return(
     <Canvas frameloop = "demand" shadows camera = {{position: [20, 3, 5], fov: 25}} gl = {{preserveDrawingBuffer: true}}>
@@ -52,7 +52,7 @@ const ComputersCanvas = ()=>{
         //these maxPolarAngle and minPolarAngle values allows us to fixed the rotation through horizontal axis only 
         minPolarAngle={Math.PI / 2}
       />
-      <Computers />
+      <Computers isMobile={isMobile} />
     </Suspense>
     <Preload all />
   </Canvas>
