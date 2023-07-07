b0a4b2f12c2d1570a6fb8ce4f94ca515a7ac6d19
rohitcodism <rohitpaula18i@gmail.com>
2023-07-07 07:21:39 +0530

    Good Morning

--- react/practices/Portfolio/src/components/canvas/Computers.jsx
+++ react/practices/Portfolio/src/components/canvas/Computers.jsx
@@ -2,6 +2,7 @@ import React, { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
+import { RGBA_ASTC_10x10_Format } from 'three';
 
 const Computers = (isMobile) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
@@ -19,8 +20,8 @@ const Computers = (isMobile) => {
       />
       <primitive
         object={computer.scene}
-        scale={ isMobile ? 0.4 : 0.75}
-        position={[0, -3.25, -1.5]}
+        scale={ isMobile ? 0.6 : 0.75}
+        position={isMobile ? [0, -3.25, -1.5] : [3, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -41,8 +42,8 @@ const ComputersCanvas = ()=>{
 
     return ()=>{
       mediaQuery.removeEventListener('change', handleMediaQueryChange);
-    }
-  }, [])
+    };
+  }, []);
 
   return(
     <Canvas frameloop = "demand" shadows camera = {{position: [20, 3, 5], fov: 25}} gl = {{preserveDrawingBuffer: true}}>
