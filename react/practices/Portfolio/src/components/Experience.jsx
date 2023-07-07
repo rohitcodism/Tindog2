46b9533065d08932ec2f778da307181db4964e64
rohitcodism <rohitpaula18i@gmail.com>
2023-07-07 21:01:26 +0530

    do it

--- react/practices/Portfolio/src/components/Experience.jsx
+++ react/practices/Portfolio/src/components/Experience.jsx
@@ -1,36 +1,9 @@
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
-import { motion } from 'framer-motion';
 
-import 'react-vertical-timeline-component/style.min.css';
-
-import { styles } from '../style';
-import { experiences } from "../constants";
-import { textVariant } from "../utils/motion";
-import { staggerContainer } from "../utils/motion";
 
 const Experience = () => {
   return (
-    <motion.section
-                variants={staggerContainer}
-                initial="hidden"
-                whileInView="show"
-                viewport={{once: true, amount: 0.25}}
-                className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-            >
-              <motion.div variants={textVariant()}>
-              <p className={styles.sectionSubText}>What I have done so far</p>
-              <h2 className={styles.sectionHeadText}>XP</h2>
-              </motion.div>
-
-              <div className="mt-20 flex flex-col">
-                <VerticalTimeline>
-                  {experiences.map((experience, index)=>(
-                    <ExperienceCard key={index} experience={experience} />
-                  ))}
-                </VerticalTimeline>
-              </div>
-            </motion.section>
+    <div>Experience</div>
   )
 }
 
-export default Experience
+export default Experience;
--- react/practices/Portfolio/src/constants/index.js
+++ react/practices/Portfolio/src/constants/index.js
@@ -23,6 +23,7 @@ import {
     jobit,
     tripguide,
     threejs,
+    sccselogo
 } from "../assets";
 
 export const navLinks = [
@@ -116,11 +117,11 @@ const technologies = [
 
 const experiences = [
     {
-        title: "React.js Developer",
-        company_name: "Starbucks",
-        icon: starbucks,
+        title: "UI Designer",
+        company_name: "SC CSE AOT",
+        icon: sccselogo,
         iconBg: "#383E56",
-        date: "March 2020 - April 2021",
+        date: "March 2023 - Present",
         points: [
             "Developing and maintaining web applications using React.js and other related technologies.",
             "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
