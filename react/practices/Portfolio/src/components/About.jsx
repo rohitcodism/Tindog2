650776dfadf01d947e88faf6c17a018cc9e0ae07
rohitcodism <rohitpaula18i@gmail.com>
2023-07-07 07:56:29 +0530

    do it

--- react/practices/Portfolio/src/components/About.jsx
+++ react/practices/Portfolio/src/components/About.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import Tilt from 'react-tilt';
 import { motion } from 'framer-motion';
 
-import { style } from '../style';
+
+import {styles} from '../style';
 import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
+import { fadeIn, textVariant } from '../utils/motion'
 
 const About = () => {
   return (
     <>
       <motion.div>
-        <p className="">Introduction</p>
-        <h2>Overview.</h2>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionSubText}>Overview.</h2>
       </motion.div>
     </>
   )
