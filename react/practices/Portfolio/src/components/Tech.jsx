c9e4192ce464811ee16102e9042a0abb868aaf9c
rohitcodism <rohitpaula18i@gmail.com>
2023-07-08 08:18:05 +0530

    do it

--- react/practices/Portfolio/src/components/Tech.jsx
+++ react/practices/Portfolio/src/components/Tech.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { BallCanvas } from "./canvas";
+import { technologies } from "../constants";
+import { staggerContainer } from "../utils/motion";
+import { motion } from "framer-motion";
+import { styles } from "../style";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <motion.section
+                variants={staggerContainer}
+                initial="hidden"
+                whileInView="show"
+                viewport={{once: true, amount: 0.25}}
+                className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+            >
+              <div className="flex flex-row flex-wrap justify-center gap-10">
+                {technologies.map((technology) =>(
+                  <div className="w-28 h-28" key={technology.name}>
+                    <BallCanvas icon={technology} />
+                  </div>
+                ) )}
+              </div>
+            </motion.section>
   )
 }
 
