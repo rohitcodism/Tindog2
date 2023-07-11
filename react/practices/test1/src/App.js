75067d95d9916e4155e440df7e6d6b6f08f01bab
rohitcodism <rohitpaula18i@gmail.com>
2023-07-11 19:50:42 +0530

    do it

--- react/practices/test1/src/App.js
+++ react/practices/test1/src/App.js
@@ -1,11 +1,28 @@
+import { useState, useReducer } from 'react';
 import './App.css';
 
+const Actions = {
+  New_todo : "add_todo"
+}
+
+function reducer( state, action ){
+
+}
+
 function App() {
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [ name, setName ] = useState('')
+
+  function handleSubmit(e){
+    
+  }
+
+
   return (
-    <div className="App flex h-screen w-full">
-      <button className='text-4xl mx-auto justify-center align-middle'> + </button>
-      <span className="text-4xl mx-auto justify-center align-middle">0</span>
-      <button className='text-4xl mx-auto justify-center align-middle'> - </button>
+    <div className="App">
+      <form action="" onSubmit={handleSubmit()} className='border-solid border-black flex-row'>
+        <input type="text" value={name} onChange={e=>setName(e.target.value)} className='border-solid border-black justify-center align-middle mt-48' />
+      </form>
     </div>
   );
 }
