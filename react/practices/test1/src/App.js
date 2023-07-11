694ccf196551b4bce919dd64463adccbcf2ed553
rohitcodism <rohitpaula18i@gmail.com>
2023-07-11 20:48:22 +0530

    do it

--- react/practices/test1/src/App.js
+++ react/practices/test1/src/App.js
@@ -6,7 +6,20 @@ const Actions = {
 }
 
 function reducer( state, action ){
+  switch(action.type){
+    case 'Actions.New_todo':
+      // eslint-disable-next-line no-undef
+      return[...todos, newTodo(action.payload.name)]
+      break;
 
+    default:
+      console.log("its default")
+  }
+}
+
+function newTodo(){
+  // eslint-disable-next-line no-restricted-globals
+  return { id: Date.now(), name: name, complete: false }
 }
 
 function App() {
@@ -14,14 +27,16 @@ function App() {
   const [ name, setName ] = useState('')
 
   function handleSubmit(e){
-    
+    dispatch({type: Actions.New_todo, payload: {name: name}})
+    setName('')
   }
 
+  console.log(todos);
 
   return (
-    <div className="App">
-      <form action="" onSubmit={handleSubmit()} className='border-solid border-black flex-row'>
-        <input type="text" value={name} onChange={e=>setName(e.target.value)} className='border-solid border-black justify-center align-middle mt-48' />
+    <div className="App w-full h-screen flex">
+      <form action="" onSubmit={()=>handleSubmit()} className='justify-center align-middle border-solid border-black'>
+        <input type="text" value={name} onChange={e=>setName(e.target.value)} className='border-solid border-black justify-center align-middle mt-48 ' />
       </form>
     </div>
   );
