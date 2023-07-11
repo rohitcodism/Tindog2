8f77717baf0079efcddc6ca7eb55b1f91ce332dc
rohitcodism <rohitpaula18i@gmail.com>
2023-07-11 14:21:39 +0530

    do it

--- react/practices/my-app/src/App.js
+++ react/practices/my-app/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useReducer } from 'react'
 
-function App() {
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Rohit
-        </a>
-      </header>
-    </div>
+    <>
+      <button> + </button>
+      <span>{ count } </span>
+      <button> - </button>
+    </>
   );
 }
-
-export default App;
