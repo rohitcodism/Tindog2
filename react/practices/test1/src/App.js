d40e4a76e731b90a2f1c8e249edaa2eb3075c09d
rohitcodism <rohitpaula18i@gmail.com>
2023-07-11 19:24:56 +0530

    do it

---
+++ react/practices/test1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className=''> + </button>
+      <span className="">0</span>
+      <button className=''> - </button>
+    </div>
+  );
+}
+
+export default App;
