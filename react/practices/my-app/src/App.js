d90b0a0f85e8a6a6581fc1191649bfd8362d87ce
rohitcodism <rohitpaula18i@gmail.com>
2023-07-11 14:30:46 +0530

    do it

--- react/practices/my-app/src/App.js
+++ react/practices/my-app/src/App.js
@@ -1,14 +1,21 @@
-import { useState, useReducer } from 'react'
+import React, { useState, useReducer } from 'react'
 
 
 export default function App() {
   const [count, setCount] = useState(0)
 
+  function increment (){
+    setCount(prevCount => prevCount + 1);
+  }
+  function decrement(){
+    setCount(prevCount => prevCount - 1);
+  }
+
   return (
     <>
-      <button> + </button>
+      <button onClick={increment()}> + </button>
       <span>{ count } </span>
-      <button> - </button>
+      <button onClick={decrement()}> - </button>
     </>
   );
 }
